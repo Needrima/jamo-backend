207b1268ab7595e81132e07a6c900c5455dbc853
Needrima <Oyebodeamirdeen@gmail.com>
2023-02-19 08:38:05 +0100

    removed mongo db connectionn string from .env file and added as an environmental variable

--- app.go
+++ app.go
@@ -39,7 +39,6 @@ func startMongo() ports.Repository {
 		helper.Config.MongoDbUserName,
 		helper.Config.MongoDbPassword,
 		helper.Config.MongoDbName,
-		// helper.Config.MongoDbConnString,
 		os.Getenv("mongo_db_conn_string"),
 	)
 
